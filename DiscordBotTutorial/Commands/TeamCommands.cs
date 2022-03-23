ad2961dfb3cc264f97538c8a44a2b45e347e3d29
Vitali Talatai <Vitali_Talatai@epam.com>
2022-03-23 15:57:30 +0300

    New: added "TeamCommands" class that contain method managing of some role.

---
+++ DiscordBotTutorial/Commands/TeamCommands.cs
@@ -0,0 +1,48 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace DiscordBotTutorial.Commands
+{
+    public class TeamCommands : BaseCommandModule
+    {
+        [Command("join")]
+        public async Task Join(CommandContext context)
+        {
+            var joinEmbed = new DiscordEmbedBuilder
+            {
+                Title = "Would you like to join?",
+                Thumbnail = new DiscordEmbedBuilder.EmbedThumbnail
+                {
+                    Url = context.Client.CurrentUser.AvatarUrl,
+                },
+                Color = DiscordColor.Red,
+            };
+
+            var joinMessage = await context.Channel.SendMessageAsync(embed: joinEmbed).ConfigureAwait(false);
+
+            var thumbsUpEmoji = DiscordEmoji.FromName(context.Client, ":+1:");
+            var thumbsDownEmoji = DiscordEmoji.FromName(context.Client, ":-1:");
+
+            await joinMessage.CreateReactionAsync(thumbsUpEmoji).ConfigureAwait(false);
+            await joinMessage.CreateReactionAsync(thumbsDownEmoji).ConfigureAwait(false);
+
+            var interactivity = context.Client.GetInteractivity();
+
+            var reactionResult = await interactivity.WaitForReactionAsync(
+                x => x.Message == joinMessage &&
+                x.User == context.User &&
+                (x.Emoji == thumbsUpEmoji || x.Emoji == thumbsDownEmoji)).ConfigureAwait(false);
+
+            if (reactionResult.Result.Emoji == thumbsUpEmoji)
+            {
+                var role = context.Guild.GetRole(902196677631430676);
+                await context.Member.GrantRoleAsync(role).ConfigureAwait(false);
+            }
+
+            await joinMessage.DeleteAsync().ConfigureAwait(false);
+        }
+    }
+}
